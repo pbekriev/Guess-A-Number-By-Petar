65c249e928434cabf3eddda6bf9c187225cd9132
pbekriev <petar.bekriev@gmail.com>
2022-11-11 20:58:54 +0200

    Update guess_a_number.py

--- guess_a_number.py
+++ guess_a_number.py
@@ -1,18 +1,26 @@
 import random
+from time import sleep
 from colorama import Fore
 
-print(Fore.BLUE + "====================RULES====================\n"
-                  "The game \"Guess the number\" has difficulty\n"
-                  "LEVELS, from which depends the size of the\n"
-                  "range in which you have to guess the number.\n"
-                  "Level 1 is to 100, Level 2 is to 200 and etc.\n"
-                  "You have 10 attempts to guess the number.\n"
-                  "=============================================")
+rules_for_print = """The game "Guess the number" has difficulty
+LEVELS, from which depends the size of the
+range in which you have to guess the number.
+Level 1 is to 100, Level 2 is to 200 and etc.
+You have 10 attempts to guess the number."""
+
+print(Fore.BLUE + "====================RULES====================")
+i = 0
+while i < len(rules_for_print):  # Print character by character
+    sleep(0.001)
+    print(Fore.BLUE + rules_for_print[i] + Fore.RESET, end='')
+    i += 1
+print()
+print(Fore.BLUE + "n=============================================" + Fore.RESET)
 
 while True:  # Main Program
 
     while True:  # Check correct input
-        level = input(Fore.RESET + "Choose LEVEL: ")
+        level = input("Choose LEVEL: ")
         if level.isdigit():
             if level != "0":
                 break
